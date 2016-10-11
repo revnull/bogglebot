11e535b97e8e013493a67733f65dffe7987f6419
Rev. Johnny Healey <rev.null@gmail.com>
2016-10-11 19:23:47 -0400

    Prints the board every 20 lines.

--- src/Game/Boggle/Bot.hs
+++ src/Game/Boggle/Bot.hs
@@ -5,6 +5,7 @@ module Game.Boggle.Bot (
                        ) where
 
 import Control.Applicative
+import Control.Lens
 import Control.Monad
 import Control.Monad.State
 import qualified Data.ByteString as BS
@@ -41,40 +42,56 @@ capPM x = x
 
 boggleBot :: Trie -> Channel -> StdGen -> IRCBot ()
 boggleBot t ch g = do
-    handleChannel ch $ flip evalStateT (g, Nothing) $ forever $ do
+    handleChannel ch $ flip evalStateT (20 :: Int, (g, Nothing)) $ forever $ do
         msg <- lift $ fmap capPM <$> readIn'
-        r <- gameRunning
-        w <- hasWarned
+        r <- zoom _2 $ gameRunning
+        w <- zoom _2 $ hasWarned
         case (msg, r, w) of
+
             (IRCMessage (PrivMsg _ _ "BOGGLE TIME"), False, _) -> do
-                void $ newGame t
+                _1 .= 20
+                void $ zoom _2 $ newGame t
                 lift $ channelTimeout ch 120000000
-                (_, Just (b, ws, _, _)) <- get
+                Just (b, ws, _, _) <- use (_2._2)
                 let ms = getSum $ foldMap (Sum . wordValue) ws
                 lift $ do
                     writeOut "It's Boggle Time!"
                     writeOut ("Maximum Score " <> fromString (show ms))
                     forM_ (boardLines b) $ \l -> do
                         writeOut l
+
             (_, False, _) -> return ()
+
             (Timeout _, _, True) -> do
-                Just (scores, missed) <- endGame
+                Just (scores, missed) <- zoom _2 $ endGame
                 lift $ writeOut "Time's up!"
                 let scores' = L.reverse $ sortBy (compare `on` snd) scores
                 forM_ scores' $ \(p, s) -> do
                     lift $ writeOut (p <> " : " <> fromString (show s))
                 forM_ (chunkWords missed) $ \miss -> do
-                    lift $ writeOut ("Missed Words: " <> BS.intercalate ", " miss)
+                    lift $ writeOut
+                        ("Missed Words: " <> BS.intercalate ", " miss)
+
             (Timeout _, _, False) -> do
-                warn
+                zoom _2 $ warn
                 lift $ writeOut "One minute remaining!"
                 lift $ channelTimeout ch 60000000
-            (IRCMessage (PrivMsg (User usr _) _ str), _, _) -> do
-                let bs = catMaybes . fmap sanitize . BS.split 32 $ str
-                mapM_ (scoreWord usr) bs
 
-                when (str == "!BOARD") $ do
-                    (_, Just (b, _, _, _)) <- get
+            (IRCMessage (PrivMsg (User usr _) _ str), _, _) -> do
+                _1 -= 1
+                lc <- use _1
+                when (lc == 0) $ do
+                    _1 .= 20
+                    Just (b, _, _, _) <- use (_2._2)
                     lift $ forM_ (boardLines b) $ \l -> do
                         writeOut l
+
+                let bs = catMaybes . fmap sanitize . BS.split 32 $ str
+                zoom _2 $ mapM_ (scoreWord usr) bs
+
+                when (str == "!BOARD") $ do
+                    Just (b, _, _, _) <- use (_2._2)
+                    lift $ forM_ (boardLines b) $ \l -> do
+                        writeOut l
+
             _ -> return ()
